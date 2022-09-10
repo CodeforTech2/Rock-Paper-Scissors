0b9b388876ecd49ff6eaffa76abacd79d8142db9
CodeforTech2 <stoineaalin2501@gmail.com>
2022-09-10 19:17:22 +0300

    declare user and computer variables

--- js.js
+++ js.js
@@ -3,3 +3,10 @@
 - paper beat rock
 - scissors beat paper
 */
+console.log("test");
+
+// Declare the user and computer variables choices
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+
