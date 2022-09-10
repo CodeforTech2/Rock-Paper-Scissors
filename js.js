eef8e6d9ad97cf7a633cefa94eb0b638a9244d6a
CodeforTech2 <stoineaalin2501@gmail.com>
2022-09-10 20:23:14 +0300

    Part of winer function

--- js.js
+++ js.js
@@ -6,8 +6,8 @@
 console.log("test");
 
 // Declare the user and computer variables choices
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+const playerSelection = "rock";//prompt("Rock, paper or scissors? Write down your answer:");
+const computerSelection = "paper"; //getComputerChoice();
 
 // Create a function to generate a random choice 
 function getComputerChoice() {
@@ -21,3 +21,19 @@ function getComputerChoice() {
     }
 };
 
+console.log(computerSelection);
+
+//Function to declare the winner
+function round(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You win! Rock beats Scissors";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return `You win! Paper beats Rock`;
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return `You win! Scissors beats Paper`;
+    } else {
+        return `It's a tie!`;
+    }
+};
+
+console.log(round(playerSelection, computerSelection));
