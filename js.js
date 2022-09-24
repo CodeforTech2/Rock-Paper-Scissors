efb4d66798bb6d4757ce76874c483ae27928d75e
Work Laptop <stoineaalin2501@gmail.com>
2022-09-24 09:22:32 +0300

    update gui rps

--- js.js
+++ js.js
@@ -1,27 +1,16 @@
-const rock = document.querySelector('#rock');
-rock.addEventListener('click', () => {
-    console.log(rock)
-});
-
-let playerSelection = '';
+const disp = document.querySelector('#display');
 const buttons = document.querySelectorAll('button');
-buttons.forEach(btn => btn.addEventListener('click', () => {
-    playerSelection = btn.id;
-    
-}))
-console.log(playerSelection);
+const player = document.querySelector('#player');
+const computer = document.querySelector('#computer');
+const playerSel = document.querySelector('#playerSelection');
+const computerSel = document.querySelector('#computerSelection');
 
-const test = document.querySelector('#test');
-test.addEventListener('click', () => {
-    console.log(test.id);
-})
+let playerScore = 0;    
+let computerScore = 0;
+let count = 0; //count the number of games played;
 
 
-
-
-
-
-// Create a function to generate a random computer choice 
+// Generate a random choice from computer rps
 function getComputerChoice() {
     let random =  Math.floor(Math.random() * 3);
     if (random === 0) {
@@ -32,51 +21,75 @@ function getComputerChoice() {
         return "scissors";
     }
 };
-
-let computerSelection = getComputerChoice();
-// console.log(computerSelection);
-
-let playerScore = 0;    
-let computerScore = 0;
-    
-//Function to declare the winner
-function round(playerSelection, computerSelection) {
-    if (
-        (playerSelection === "rock" && computerSelection === "scissors") ||
-        (playerSelection === "paper" && computerSelection === "rock") ||
-        (playerSelection === "scissors" && computerSelection === "paper")
-    ) {
-        playerScore++;
-        return `You win! ${playerSelection} beats ${computerSelection}!`;
-    } else if (
-        (playerSelection === "rock" && computerSelection === "paper") ||
-        (playerSelection === "paper" && computerSelection === "scissors") ||
-        (playerSelection === "scissors" && computerSelection === "rock")
-    ) {
-        computerScore++;
-        return `You loose! ${computerSelection} beats ${playerSelection}!`;
-    }  else {
-        return `It's a tie!`;
+//Function that end game and announce the winner and the final score
+function endGame() {
+    if (playerScore > computerScore) {
+        console.log(`Player win with: ${playerScore}-${computerScore}`);
+    } else if (computerScore > playerScore) {
+        console.log(`Computer win with: ${computerScore}-${playerScore}!`);
+    } else {
+        console.log("It's a tie!");
     }
 };
 
-//Function that play 5 rounds and keeps score and reports winners
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = "rock"; //prompt("Rock, paper or scissors? Write down your answer:");
-        let computerSelection = getComputerChoice();
-        round();
-        console.log(i + 1, `Player: ${playerSelection} - Computer: ${computerSelection}`);
-        console.log( round(playerSelection, computerSelection), playerScore, computerScore);
-    }
-    //console.log(playerScore, computerScore);
-        if (playerScore > computerScore) {
-            console.log(`Player win with: ${playerScore}-${computerScore}`);
-        } else if (computerScore > playerScore) {
-            console.log(`Computer win with: ${computerScore}-${playerScore}!`);
-        } else {
-            console.log("It's a tie!");
+// buttons.onclick = function () {
+//     // count++;
+//     console.log(buttons[0]);
+//     // disp.innerHTML = count;
+// };
+
+buttons.forEach(btn => btn.addEventListener('click', () => {
+    count++;
+    console.log(count);
+    disp.innerHTML = count;
+    function playRound(playerSelection, computerSelection) {
+        if (
+            (playerSelection === "rock" && computerSelection === "scissors") ||
+            (playerSelection === "paper" && computerSelection === "rock") ||
+            (playerSelection === "scissors" && computerSelection === "paper")
+        ) {
+            playerScore++;
+            player.innerHTML = playerScore;
+            return `You win! ${playerSelection} beats ${computerSelection}!`;
+        } else if (
+            (playerSelection === "rock" && computerSelection === "paper") ||
+            (playerSelection === "paper" && computerSelection === "scissors") ||
+            (playerSelection === "scissors" && computerSelection === "rock")
+        ) {
+            computerScore++;
+            computer.innerHTML = computerScore;
+            return `You loose! ${computerSelection} beats ${playerSelection}!`;
+        }  else {
+            return `It's a tie!`;
         }
-};
+    };
 
-console.log(game());
+    function game() {
+        if (count <= 5) {
+            let playerSelection = btn.id;
+            let computerSelection = getComputerChoice();
+            playRound();
+            console.log(`Player: ${playerSelection} - Computer: ${computerSelection}`);
+            console.log( playRound(playerSelection, computerSelection), playerScore, computerScore);
+        };        
+    };
+    
+
+        endGame();//ceva nu merge bine la functia asta, nu imi arata rezultatul final cu conditia asta if count == x
+    
+    console.log(game());
+}));
+
+
+/*let count = 0;
+const btn = document.querySelector('#btn');
+const disp = document.querySelector('#display');
+
+// btn.onclick = function () {
+//     count++;
+//     disp.innerHTML = count;
+// }
+btn.addEventListener('click', () => {
+    count++;
+    // disp.innerHTML = count;
+})*/
